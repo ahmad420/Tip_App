550b06d9e81699edc1de5a859e95710b9415ca82
Ahmad Af <ahmdaf420@gmail.com>
2021-07-18 04:13:26 +0300

    app crash fixed and icons added .

--- app/src/main/java/com/example/tip_app/MainActivity.kt
+++ app/src/main/java/com/example/tip_app/MainActivity.kt
@@ -8,7 +8,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-     lateinit var binding: ActivityMainBinding
+     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +17,22 @@ class MainActivity : AppCompatActivity() {
        binding.calculateButton.setOnClickListener {calculateTip()}
     }
 
-     fun calculateTip() {
+     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-         val selectedId =binding.tipOptions.checkedRadioButtonId
-         val tipPercentage = when(selectedId){
+         val cost = stringInTextField.toDoubleOrNull()
+         if (cost == null) {
+             binding.tipAmount.text = ""
+             return
+         }
+         val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
              R.id.amazing_service_radioButton -> 0.20
              R.id.good_service_radioButton -> 0.18
              else ->0.15
          }
-         var tip = tipPercentage * cost
+         var tip: Double = tipPercentage * cost
 
-         val roundUp = binding.RoundTipSwitch.isChecked
-         if(roundUp){
-             tip = kotlin.math.ceil(tip)
-
-         }
+         val roundUp: Boolean = binding.RoundTipSwitch.isChecked
+         if(roundUp) tip = kotlin.math.ceil(tip)
 
          val formattedTip =  NumberFormat.getCurrencyInstance().format(tip)
          binding.tipAmount.text = getString(R.string.tip_amount , formattedTip)
